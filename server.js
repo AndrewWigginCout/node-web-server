fdda588ff84174a78055cf9e2ec5a6d6ab54b70e
= <AndrewWiggin@example.com>
2022-07-28 20:22:03 -0700

    relpath

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const http = require("http");
 const fs = require('fs');
 console.log("__dirname=", __dirname)
-function generate(folderPath) {
-  //folderPath = folderPath;
-  console.log("foldePath=", folderPath);
+function generate(root, relpath) {
+  console.log("root=",root);
+  console.log("relpath=",relpath);
   head = "<title>dir</title>";
-  files = fs.readdirSync(folderPath);
+  files = fs.readdirSync(root+relpath);
   html=`<html>
   <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Directory listing for ${folderPath}</title>
+  <title>Directory listing for ${relpath}</title>
   </head>
   <body>
-  <h1>Directory listing for ${folderPath}</h1>
+  <h1>Directory listing for ${relpath}</h1>
   <hr>
   <ul>`;
   files.forEach(file => {
-    var stat = fs.statSync(folderPath+file);
+    console.log("file=", file);
+    var stat = fs.statSync(root+relpath+file);
     if (stat && stat.isDirectory()) {
       html+=`<li><a href="${file}/">d ${file}/</a></li>\n`;
     } else {
@@ -34,22 +35,22 @@ function generate(folderPath) {
 
 const host = 'localhost';
 const port = 8080;
-const root = '/run/media/public/sdc2/_s/_ks/music'
+const root = '/run/media/public/sdb2/_s/_ks/music'
 
 const requestListener = function (req, res) {
   console.log("req.url=", req.url);
-  var path = decodeURIComponent(req.url);
-  console.log("path=", path)
+  var relpath = decodeURIComponent(req.url);
+  console.log("relpath=", relpath)
     
-  var stat = fs.statSync(root + path);
+  var stat = fs.statSync(root + relpath);
   if (stat && stat.isDirectory()){
     console.log("is directory");
     res.setHeader("Content-Type", "text/html");
     res.writeHead(200);
-    res.end(generate(root + path));
+    res.end(generate(root,relpath));
   } else{
     console.log("else");
-    var contents = fs.readFileSync(root + path);
+    var contents = fs.readFileSync(root + relpath);
     //res.setHeader("Content-Type", "text/html")
     res.writeHead(200);
     res.end(contents);
